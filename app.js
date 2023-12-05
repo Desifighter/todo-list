3cae133b8d793146b364db7b2a7a4aba1cb2b235
keshav keshari <kg1718129@gmail.com>
2023-12-05 18:01:13 +0530

    update for cyclic

--- app.js
+++ app.js
@@ -20,8 +20,74 @@ const datesSchema = new mongoose.Schema({
 const dates = mongoose.model("dates", datesSchema);
 const work = mongoose.model("worklist", datesSchema);
 
+
 let task = [];
 let workList = [];
+
+
+// te sahi kam kar raha hai
+// deleteTaskAtIndex(dates,date.getFDate(),1);
+async function deleteTaskAtIndex(Model, title, indexToRemove) {
+  try {
+    const document = await Model.findOne({ title: title });
+
+    console.log(document);
+
+    if (!document) {
+      console.log("Document not found");
+      return;
+    }
+
+    if (document.tasks && document.tasks[indexToRemove]) {
+      document.tasks.splice(indexToRemove, 1);
+      await document.save();
+      console.log("Element removed successfully");
+    } else {
+      console.log("Invalid index or task not found");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+app.post("/delete", (req, res) => {
+  let originalString = req.body.checkbox;
+
+  console.log("original string = "+originalString);
+
+  let substringToRemove = date.getDate();
+
+  if (originalString.includes("Work List")) {
+    substringToRemove = "Work List";
+  }
+
+  console.log("String to Remove = " + substringToRemove);
+
+  const trimmedString = originalString.replace(substringToRemove, "");
+
+  console.log("Trimmed String = "+trimmedString);
+  const index = parseInt(trimmedString, 10);
+
+  console.log("Index = "+index+"  type of index = "+(typeof index));
+
+  
+
+  if (!isNaN(index) && index >= 0) {
+    console.log(index);
+
+    if (originalString.includes("Work List")) {
+      deleteTaskAtIndex(work, "worktask", index);
+      res.redirect("/work");
+    } else {
+      deleteTaskAtIndex(dates, date.getFDate(), index);
+      res.redirect("/");
+    }
+  } else {
+    console.error("Invalid index");
+    res.status(400).send("Invalid index");
+  }
+});
 
 async function refreshData(Model, title) {
   try {
@@ -33,7 +99,7 @@ async function refreshData(Model, title) {
       } else {
         task = tasksArray;
       }
-      console.log(tasksArray);
+      // console.log(tasksArray);
     } else {
       const addDate = new Model({
         title: title,
@@ -51,7 +117,7 @@ async function refreshData(Model, title) {
 
 app.get("/", async (req, res) => {
   await refreshData(dates, date.getFDate());
-  console.log(task);
+  // console.log(task);
   res.render("index", { day: date.getDate(), task: task });
 });
 
@@ -61,9 +127,13 @@ app.get("/about", (req, res) => {
 
 app.get("/work", async (req, res) => {
   await refreshData(work, "worktask");
-  console.log(workList);
+  // console.log(workList);
   res.render("index", { day: "Work List", task: workList });
 });
+
+
+
+
 
 app.post("/", async (req, res) => {
   let element = req.body.list;
@@ -95,6 +165,8 @@ async function pushElementInArr(Model, titleToUpdate, taskToAdd) {
     throw err; // Re-throw the error for the caller to handle
   }
 }
+
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Example app listening on port " + PORT));
